3a4cecf9c7bc2fdfbe5bdaee1d766fd506c23f98
miogy <jjangrl87@gmail.com>
2022-10-11 19:20:13 +0900

    modified: 불필요한 주석등 정리

--- src/Router.js
+++ src/Router.js
@@ -1,20 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from './component/Header';
-import Main from './page/Main';
+import Header from "./component/Header";
+import Main from "./page/Main";
 
-function Router (){
+function Router() {
   return (
-    <>
     <BrowserRouter>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Main />}></Route>
-      <Route path="/detail" element={<div>상세</div>}></Route>
-      <Route path="/about" element={<div>about</div>}></Route>
-    </Routes>
-    {/* <Footer /> */}
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/detail" element={<div>상세</div>}></Route>
+        <Route path="/about" element={<div>about</div>}></Route>
+      </Routes>
+      {/* <Footer /> */}
     </BrowserRouter>
-    </> // Route : 페이지를 의미함.
-  )
+  );
 }
 export default Router;
